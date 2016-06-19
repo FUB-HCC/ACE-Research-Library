636ba17e8bd836084926ca792feb3a47372f3611
Denis Drescher <drescher@ferret-go.com>
2016-06-19 15:12:34 +0200

    Rearranged admin files and classes

---
+++ researchlibrary/api/admin/utils.py
@@ -0,0 +1,62 @@
+import re
+from haystack.forms import SearchForm
+from stop_words import get_stop_words
+from readability.readability import Document
+from utilofies.stdlib import cached_property
+
+class Gist:
+
+    keyword_pattern = re.compile(r'^[^\d]+$')
+    stop_words = set(get_stop_words('en'))
+
+    def __init__(self, html):
+        self.html = html
+        self.document = Document(html)
+
+    @property
+    def title(self):
+        return self.document.short_title()
+
+    @cached_property
+    def text(self):
+        text = self.document.summary()
+        text = re.sub('<br[^>]+>', '\n', text)
+        text = re.sub('</?p[^>]+>', '\n\n', text)
+        text = re.sub('<[^>]+>', '', text)
+        text = re.sub('^[ \t]+$', '', text)
+        text = re.sub('\n{3,}', '\n\n', text, flags=re.MULTILINE)
+        return text
+
+    @staticmethod
+    def _common_prefix(one, two):
+        parallelity = [x == y for x, y in zip(one, two)] + [False]
+        return parallelity.index(False)
+
+    @classmethod
+    def _find_representative(cls, stem, text):
+        tokens = text.split()
+        prefixes = {token: cls._common_prefix(token, stem) for token in tokens}
+        best = lambda token: (-token[1], len(token[0]))
+        return sorted(prefixes.items(), key=best)[0][0]
+
+    @classmethod
+    def _is_good_keyword(cls, word):
+        return (word not in cls.stop_words) and \
+                cls.keyword_pattern.match(word)
+
+    @classmethod
+    def find_keywords(cls, text):
+        whoosh_backend = SearchForm().searchqueryset.query.backend
+        if not whoosh_backend.setup_complete:
+            whoosh_backend.setup()
+        with whoosh_backend.index.searcher() as searcher:
+            keywords = searcher.key_terms_from_text(
+                'text', text, numterms=10, normalize=False)
+        keywords = list(zip(*keywords))[0]
+        keywords = [cls._find_representative(keyword, text) for keyword in keywords]
+        keywords = [keyword for keyword in keywords if cls._is_good_keyword(keyword)]
+        return keywords
+
+    @property
+    def keywords(self):
+        return self.find_keywords(self.text)
---
+++ researchlibrary/api/admin/widgets.py
@@ -0,0 +1,94 @@
+import json
+from django.conf import settings
+from django.forms import Media
+from django.utils.safestring import mark_safe
+from django_select2.forms import ModelSelect2TagWidget
+from ..models import Person, Keyword
+
+
+class ModelSelect2TagWidgetBase(ModelSelect2TagWidget):
+
+    def get_url(self):
+        """
+        Get noting at all.
+
+        Django Select2 abuses the cache to inject autosuggest values into the
+        admin view via AJAX. That’s terrible. Since we don’t use that view,
+        this method that references it needs to be overwritten.
+        """
+        pass
+
+    def build_attrs(self, extra_attrs=None, **kwargs):
+        """
+        Remove the data attributes for the AJAX call we don’t use and
+        customize a bit more.
+        """
+        attrs = super().build_attrs(extra_attrs=extra_attrs, **kwargs)
+        # Originally:
+        # {'data-token-separators': '[",", " "]',
+        #  'data-minimum-input-length': 1,
+        #  'data-tags': 'true',
+        #  'data-ajax--type': 'GET',
+        #  'data-ajax--cache': 'true',
+        #  'data-ajax--url': '/select2/fields/auto.json',
+        #  'class': 'django-select2 django-select2-heavy',
+        #  'name': 'authors',
+        #  'id': 'id_authors',
+        #  'data-field_id': 'MTQwNTg5NTg3NzM3NTEy:1bD5p3:suZhZBqbvSzNpEMGve-KGjfmffw',
+        #  'data-allow-clear': 'false'}
+        attrs['data-token-separators'] = r'[","]'
+        del attrs['data-ajax--url']
+        del attrs['data-ajax--type']
+        del attrs['data-ajax--cache']
+        return attrs
+
+    def render(self, name, value, attrs=None, choices=()):
+        output = super().render(name, value, attrs, choices)
+        # Let’s think of something new if and when the page reaches 1+ MiB.
+        output += """
+            <p class="help">Complete tags by hitting enter or comma.</p>
+            <script type="text/javascript">
+                var select = $('#%s');
+                select.select2({
+                    createTag: function(params) {
+                        return {id: -1, text: params.term}
+                    },
+                    tags: true,
+                    // selectOnClose: true,  // Too much recursion error
+                    data: %s
+                });
+                select.on('select2:select', register('%s'));
+            </script>\n
+        """ % (
+            attrs['id'],
+            json.dumps([
+                {'id': obj.pk, 'text': getattr(obj, self.field)}
+                for obj in self.model.objects.all()]),
+            self.model._meta.model_name)
+        return mark_safe(output)
+
+    @property
+    def media(self):
+        """
+        Construct Media as a dynamic property.
+        .. Note:: For more information visit
+            https://docs.djangoproject.com/en/1.8/topics/forms/media/#media-as-a-dynamic-property
+        """
+        return Media(
+            js=(settings.SELECT2_JS,),
+            css={'screen': (settings.SELECT2_CSS,)})
+
+
+class PersonModelSelect2TagWidget(ModelSelect2TagWidgetBase):
+    model = Person
+    field = 'name'
+
+
+class KeywordModelSelect2TagWidget(ModelSelect2TagWidgetBase):
+    model = Keyword
+    field = 'name'
+
+    def render(self, name, value, attrs=None, choices=()):
+        output = super().render(name, value, attrs, choices)
+        output += 'Suggestions: <span id="keyword_suggestions"></span>'
+        return mark_safe(output)
