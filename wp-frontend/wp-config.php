4d29ddc942ed31509e8d9bff0de5eb19314b1d50
Denis Drescher <drescher@ferret-go.com>
2016-06-29 11:35:47 +0200

    Changed MySQL login

--- wp-frontend/wp-config.php
+++ wp-frontend/wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'tarix');
+define('DB_USER', 'wordpress');
 
 /** MySQL database password */
-define('DB_PASSWORD', '12');
+define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
