e57c0aee6f3bbc1707d6230e2a003e8b0ca2536e
Denis Drescher <drescher@ferret-go.com>
2016-07-21 18:12:39 +0200

    This should no longer be needed

--- wp-frontend/wp-content/themes/genesis/js/config_app.js
+++
@@ -1,7 +0,0 @@
-var researchLibrary = angular.module('researchLibrary', ['ngMaterial','ngAnimate','ui.bootstrap'])
-    .constant('config', {
-//        url: 'http://nbi-srv03.imp.fu-berlin.de/api/v1/'});
-        url: 'http://acerl.claviger.net/api/v1/'});
-//        url: 'http://192.168.151.128/wordpress/index.php/researchlibrary/api/'});
-//        url: 'api/v1/'});
-
