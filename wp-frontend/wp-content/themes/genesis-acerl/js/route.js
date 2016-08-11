6e954e31aac71b4eac9b201c43436e79cfeb4ca6
Denis Drescher <drescher@ferret-go.com>
2016-08-11 09:34:38 +0200

    Fixed paths

--- wp-frontend/wp-content/themes/genesis-acerl/js/route.js
+++ wp-frontend/wp-content/themes/genesis-acerl/js/route.js
@@ -4,8 +4,8 @@
 researchLibrary.config(['$routeProvider', function($routeProvider) {
 
     $routeProvider.
-    when('/search', {templateUrl: '../../wp-content/themes/genesis-acerl/search.html'}).
-    when('/searchfull', {templateUrl: '../../wp-content/themes/genesis-acerl/searchfull.html'}).
+    when('/search', {templateUrl: '/wordpress/wp-content/themes/genesis-acerl/search.html'}).
+    when('/searchfull', {templateUrl: '/wordpress/wp-content/themes/genesis-acerl/searchfull.html'}).
 
     otherwise({redirectTo: '/search'});
 
