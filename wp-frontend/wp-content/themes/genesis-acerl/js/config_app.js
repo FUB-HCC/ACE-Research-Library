7d4a855296cc9cbf1bd0452c5181c301ab50e486
tarix <taras.kolba@fu-berlin.de>
2016-09-14 16:00:25 +0200

    improve View from search without Searchitem

--- wp-frontend/wp-content/themes/genesis-acerl/js/config_app.js
+++ wp-frontend/wp-content/themes/genesis-acerl/js/config_app.js
@@ -3,5 +3,4 @@ var researchLibrary = angular.module('researchLibrary', ['ngRoute','ngMaterial',
 //        url: 'http://nbi-srv03.imp.fu-berlin.de/api/v1/'});
         url: 'http://acerl.claviger.net/api/v1/'});
 //        url: 'http://192.168.151.128/wordpress/index.php/researchlibrary/api/v1/'});
-//        url: 'api/v1/'});
-
+//        url: 'http://192.168.151.128:8011/api/v1/'});
