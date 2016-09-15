3377446d7cef0b1c14c7b34855c069610961c857
Benjamin Kahl <benjaminkahl19@gmail.com>
2016-09-15 16:50:56 +0200

    Fixed minor formatting mistake in search-parameters

--- researchlibrary/api/views.py
+++ researchlibrary/api/views.py
@@ -106,7 +106,7 @@ class SearchViewSet(viewsets.GenericViewSet):
         return {
             'relevance' : queryset.order_by(),
             '-date' : queryset.order_by('-published'),
-            '+date' : queryset.order_by('published'),
+            'date' : queryset.order_by('published'),
             'pubtype' : queryset.order_by('resource_type'),
         }.get(sorting, queryset.order_by())
 
